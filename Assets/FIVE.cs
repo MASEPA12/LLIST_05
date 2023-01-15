e2562d1e3cbf2c5c34505a40135b818170edb14b
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-01-15 18:22:28 +0100

    exercise 4 and 5
    finished

--- Assets/FIVE.cs
+++ Assets/FIVE.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FIVE : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //here's the array with no defined lenght
+    public int[] ARRAY;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            //if the lenght of the array is equal to 5 or bigger:
+            if (ARRAY.Length >= 5)
+            {
+                //we access the number 1,3 and 5 of the array through;
+                Debug.Log($"First element = {ARRAY[1]}");
+                Debug.Log($"Third element = {ARRAY[2]}");
+                Debug.Log($"Fifth element = {ARRAY[3]}");
+            }
+            else //if it's less than 5:
+            {
+                Debug.Log("Missing elements");
+            }
+        }
     }
 }
--- Assets/FOUR.cs
+++ Assets/FOUR.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class FOUR : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //here's the array with no defined lenght
+    public int[] ARRAY;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            //we access the number 1,3 and 5 of the array through;
+            Debug.Log($"First element = {ARRAY[1]}");
+            Debug.Log($"Third element = {ARRAY[2]}");
+            Debug.Log($"Fifth element = {ARRAY[3]}");
+        }
     }
 }
